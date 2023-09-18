3958abd9e215770f1cebe4b48610a3f1bb445d45
martinzuro <martinzuro@gmail.com>
2023-09-18 14:09:12 +0000

    enlarge indicator circle size

--- resources/js/components/Indicator.vue
+++ resources/js/components/Indicator.vue
@@ -1,7 +1,7 @@
 <template>
     <span class="whitespace-no-wrap" v-if="!field.shouldHide">
         <span
-            class="inline-block indicator-grey rounded-full w-2 h-2 mr-1"
+            class="inline-block indicator-grey rounded-full w-3 h-3 mr-1"
             v-bind="colorClassStyle"
         />
         <span v-if="labelText">
